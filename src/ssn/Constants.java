27fdb37a325f06d0e86e87953484bad9b7d6cb49
alieus <alieus@hotmail.gr>
2016-04-07 15:43:14 +0300

    Introduce default connection constatnts

--- src/ssn/Constants.java
+++ src/ssn/Constants.java
@@ -2,4 +2,13 @@ package ssn;
 
 public interface Constants {
     static final int REDUCE_LIMIT = 10;
+    
+    static final String DEFAULT_DATASOURCE_URL = "83.212.117.76/ds_systems_2016";
+    static final String DEFAULT_DATASOURCE_USERNAME = "omada35";
+    static final String DEFAULT_DATASOURCE_PASSWORD = "omada35db";
+    
+    static final int DEFAULT_MASTER_CLIENT_PORT = 25697;
+    static final int DEFAULT_MASTER_CONTROL_PORT = 25698;
+    static final int DEFAULT_REDUCER_PORT = 25699;
+    static final int DEFAULT_MAPPER_PORT = 25700;
 }
