d100e99eaad2fa105ef6c35cdd374fdfadc0c5f6
alieus <alieus@hotmail.gr>
2016-04-07 22:03:20 +0300

    Create general Request class

---
+++ src/ssn/models/Request.java
@@ -0,0 +1,65 @@
+package ssn.models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Request {
+    private String action;
+    private String body;
+
+    public Request() {
+    }
+
+    public Request(String action, String body) {
+        this.action = action;
+        this.body = body;
+    }
+    
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getBody() {
+        return body;
+    }
+    
+    public <T> T getBodyAs(Class<T> clazz) {
+        ObjectMapper m = new ObjectMapper();
+        try {
+            return m.readValue(body, clazz);
+        } catch (IOException ex) {
+            throw new IllegalStateException("Request body object could not be converted to JSON", ex);
+        }
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            ObjectMapper m = new ObjectMapper();
+            return m.writeValueAsString(this);
+        } catch (JsonProcessingException ex) {
+            throw new IllegalStateException("Request could not be converted to JSON", ex);
+        }
+    }
+    
+    public static Request fromObject(String action, Object body) {
+        try {
+            ObjectMapper m = new ObjectMapper();
+            return new Request(action, m.writeValueAsString(body));
+        } catch (JsonProcessingException ex) {
+            throw new IllegalArgumentException("Given body object could not be converted to JSON", ex);
+        }
+    }
+    
+}
