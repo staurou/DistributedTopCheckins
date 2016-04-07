50d990fafb69de84165a44bc83d0e590b6c88deb
alieus <alieus@hotmail.gr>
2016-04-07 22:04:00 +0300

    Rename Reply to LocationStatsReply

---
+++ src/ssn/models/LocationStatsReply.java
@@ -0,0 +1,40 @@
+package ssn.models;
+
+public class LocationStatsReply {
+    private double longitude;
+    private double latitude;
+    private int counts;
+    private String POI;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getCounts() {
+        return counts;
+    }
+
+    public void setCounts(int counts) {
+        this.counts = counts;
+    }
+
+    public String getPOI() {
+        return POI;
+    }
+
+    public void setPOI(String POI) {
+        this.POI = POI;
+    }
+}
