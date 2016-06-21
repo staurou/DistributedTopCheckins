c9e52ec80e05fc577aad564a490550a22e80ce96
alistat <alieus@hotmail.gr>
2016-06-21 04:01:15 +0300

    Everything working

--- Android/DistributedTopCheckins/app/src/main/java/ssn/distributedtopcheckins/MainActivity.java
+++ Android/DistributedTopCheckins/app/src/main/java/ssn/distributedtopcheckins/MainActivity.java
@@ -1,11 +1,17 @@
 package ssn.distributedtopcheckins;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,11 +31,19 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public static final int TAKE_PICTURE = 0;
 
-    class JsHandle {
+
+    public class JsHandle {
         WebView webView;
 
         public JsHandle(WebView webView) {
@@ -37,35 +51,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @JavascriptInterface
-        void uploadPhoto() {
+        public void uploadPhoto() {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                // Ensure that there's a camera activity to handle the intent
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    // Create the File where the photo should go
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    // Ensure that there's a camera activity to handle the intent
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        // Create the File where the photo should go
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                        // Continue only if the File was successfully created
+                        if (photoFile != null) {
+                            Uri photoURI = Uri.fromFile(photoFile);
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            waitingPhoto = true;
+                            startActivityForResult(takePictureIntent, TAKE_PICTURE);
+                        }
                     }
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        Uri photoURI = Uri.fromFile(photoFile);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        waitingPhoto = true;
-                        startActivityForResult(takePictureIntent, TAKE_PICTURE);
-                    }
-                }
 
                 }
             });
         }
 
         @JavascriptInterface
-        String getPhotoData() {
+        public String getPhotoData() {
             return readAsBase64(mCurrentPhotoPath);
         }
     }
@@ -74,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
     private String server;
     private boolean waitingPhoto;
     private boolean reloadedWhileWaitingPhoto;
+    private LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -81,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalCacheDir();
 //        File storageDir = new File("/");
+        Log.d("APP", storageDir+"");
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -111,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             final WebView webview = (WebView) findViewById(R.id.webView);
             final String command = !reloadedWhileWaitingPhoto ?
                     "receivePhoto();" :
-                    "window.setTimeout(function () {alert('haha');receivePhoto();}, 2000);";
+                    "window.setTimeout(function () {receivePhoto();}, 2000);";
             assert webview != null;
             if (reloadedWhileWaitingPhoto) {
                 webview.postDelayed(new Runnable() {
@@ -160,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onConsoleMessage(ConsoleMessage cm) {
                 Log.d("APP", cm.message() + " -- From line "
                         + cm.lineNumber() + " of "
-                        + cm.sourceId() );
+                        + cm.sourceId());
                 return true;
             }
 
@@ -175,13 +192,41 @@ public class MainActivity extends AppCompatActivity {
 
         webview.addJavascriptInterface(new JsHandle(webview), "Android");
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    10);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        11);
+        }
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(2000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+
         if (savedInstanceState != null) {
             mCurrentPhotoPath = savedInstanceState.getString("mCurrentPhotoPath");
             server = savedInstanceState.getString("server");
             waitingPhoto = savedInstanceState.getBoolean("waitingPhoto");
             if (waitingPhoto) reloadedWhileWaitingPhoto = true;
         } else {
-            if (!((LocationManager) getSystemService( LOCATION_SERVICE )).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (!((LocationManager) getSystemService(LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                 Toast.makeText(this, "For best results enable GPS", Toast.LENGTH_LONG).show();
             }
         }
@@ -205,5 +250,48 @@ public class MainActivity extends AppCompatActivity {
             webview.loadUrl(server);
         }
     }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+        final WebView webview = (WebView) findViewById(R.id.webView);
+        webview.post(new Runnable() {
+            @Override
+            public void run() {
+                webview.evaluateJavascript("geo_success({coords: {latitude:"+location.getLatitude()
+                        +",longitude:"+location.getLongitude()+",accuracy:"+location.getAccuracy()+"}})", null);
+            }
+        });
+    }
+
 
 }
